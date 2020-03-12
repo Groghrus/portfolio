b99989e66ffa78bb5834e15474d4759279a00aca
Dmitriy Kiselev <sckdv1988@gmail.com>
2020-03-12 15:36:10 +0300

    add about page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuLogo = document.querySelector('.menu-logo');
 const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
@@ -13,13 +13,15 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        menuLogo.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        menuLogo.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         showMenu = false;
