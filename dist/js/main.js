6c2b8a7c6fe44277225656af447a61a6bb031cb5
Dmitriy Kiselev <sckdv1988@gmail.com>
2020-03-31 20:42:23 +0300

    add

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,11 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const menuNav = document.querySelector('.menu-nav');
         const menuLogo = document.querySelector('.menu-logo');
         const navItems = document.querySelectorAll('.nav-item');
-
         let showMenu = false;
-
         menuBtn.addEventListener('click', toggleMenu);
-
         function toggleMenu() {
             if (!showMenu) {
                 menuBtn.classList.add('close');
@@ -33,28 +30,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     }
-
-   /* {
-        //form
-        //валидация формы
-
-        let inputEmail = document.querySelector('.input__email');
-        let emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-        inputEmail.onblur = function () {
-            let test = inputEmail.value.length === 0 || emailRegExp.test(inputEmail.value);
-            if (!test){
-                inputEmail.classList.add('invalid');
-                inputEmail.classList.remove('line-bottom');
-            }
-        };
-        inputEmail.onfocus = function () {
-            if (this.classList.contains('invalid')){
-                this.classList.remove('invalid');
-            }
-        };
-
-
-    }*/
-
 });
